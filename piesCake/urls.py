8961a9f31d6ab240fc806bb1007f6b44ef57ec94
ABHISHEK KUMAR <kumarpuplish@gmail.com>
2025-04-20 02:10:49 +0530

    changes for the static files

--- piesCake/urls.py
+++ piesCake/urls.py
@@ -18,7 +18,6 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns = [
     path('admin/', include('admin.urls')),
     path('api/', include('apis.urls')),
