487b6d9c4a9dc715eff0284302baa62d56bfa58e
ABHISHEK KUMAR <kumarpuplish@gmail.com>
2025-05-19 08:39:59 +0530

    Gst changes

--- piesCake/settings.py
+++ piesCake/settings.py
@@ -122,23 +122,23 @@ CORS_ALLOWED_ORIGINS = [
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'piesCakes',
-        'USER': 'pies_cakes',
-        'PASSWORD': '6Y/L/bD5ZvrM23g',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'piesCakes',
+#         'USER': 'pies_cakes',
+#         'PASSWORD': '6Y/L/bD5ZvrM23g',
+#         'HOST': 'localhost',
+#         'PORT': '3306',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
